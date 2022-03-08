0401f86fd031aa55da921b083b2322b6d41a9b12
nctamer <nazifcan.tamer@upf.edu>
2022-03-08 22:13:06 +0100

    Apply timbre outlier check during synthesis

--- synth.py
+++ synth.py
@@ -38,11 +38,13 @@ def silence_segments_one_run(confidences, confidence_threshold, segment_len_th):
     return voiced
 
 
-def silence_unvoiced_segments(pitch_track_csv, confidence_threshold=0.2, min_voiced_segment_ms=12):
+def silence_unvoiced_segments(pitch_track_csv, low_confidence_threshold=0.2,
+                              high_confidence_threshold=0.7, min_voiced_segment_ms=12):
     """
     Accepts crepe output in the csv format and removes unvoiced segments with confidence and accepted voiced segment duration
     :param pitch_track_csv: csv with [ºtimeº, ºfrequencyº, ºconfidenceº] fields
-    :param confidence_threshold: confidence threshold in range (0,1)
+    :param low_confidence_threshold: confidence threshold in range (0,1)
+    :param high_confidence_threshold: confidence threshold in range (0,1)
     :param min_voiced_segment_ms: voiced segments shorter than the specified lenght are discarded
     :return: input csv file with the silenced segments
     """
@@ -50,11 +52,13 @@ def silence_unvoiced_segments(pitch_track_csv, confidence_threshold=0.2, min_voi
     voiced_th = int(np.ceil(min_voiced_segment_ms/annotation_interval_ms))
 
     # we do not accept the segment if a close neighbors do not have a confidence > 0.7
-    smoothened_confidences = medfilt(pitch_track_csv["confidence"], kernel_size=11)
-    smooth_voiced = silence_segments_one_run(smoothened_confidences, confidence_threshold=0.7, segment_len_th=voiced_th)
+    smoothened_confidences = medfilt(pitch_track_csv["confidence"], kernel_size=2*(voiced_th//2)+1)
+    smooth_voiced = silence_segments_one_run(smoothened_confidences,
+                                             confidence_threshold=high_confidence_threshold, segment_len_th=voiced_th)
 
     # we also do not accept the pitch values if the individual confidences are really low
-    hard_voiced = silence_segments_one_run(pitch_track_csv["confidence"], confidence_threshold=confidence_threshold, segment_len_th=voiced_th)
+    hard_voiced = silence_segments_one_run(pitch_track_csv["confidence"],
+                                           confidence_threshold=low_confidence_threshold, segment_len_th=voiced_th)
     
     # we accept the intersection of these two zones
     voiced = np.logical_and(smooth_voiced, hard_voiced)
@@ -65,7 +69,7 @@ def silence_unvoiced_segments(pitch_track_csv, confidence_threshold=0.2, min_voi
         # we don't want small unvoiced zones. Check if they are acceptable with a more favorable mean thresholding
         if np.diff(unvoiced_boundary) < voiced_th:
             avg_convidence = pitch_track_csv.loc[unvoiced_boundary[0]:unvoiced_boundary[1], "confidence"].mean()
-            if avg_convidence > confidence_threshold:
+            if avg_convidence > low_confidence_threshold:
                 voiced[unvoiced_boundary[0]:unvoiced_boundary[1]] = True
 
     pitch_track_csv.loc[~voiced, "frequency"] = 0
@@ -85,7 +89,6 @@ def interpolate_f0_to_sr(pitch_track_csv, audio, sr=SAMPLING_RATE, hop_size=HOP_
     pitch_track_np = f(time)
     confidence_np = c(time)
     pitch_track_np[pitch_track_np < 10] = 0  # interpolation might introduce odd frequencies
-    confidence_np[pitch_track_np < 10] = 0
     return pitch_track_np, confidence_np, time
 
 
@@ -107,7 +110,7 @@ def anal(audio, f0, hop_size=HOP_SIZE, sr=SAMPLING_RATE):
     return hfreq, hmag, hphase
 
 
-def refine_harmonics_twm(hfreq, hmag, hphases, f0, f0et=5.0, f0_refinement_range_cents=10):
+def refine_harmonics_twm(hfreq, hmag, hphases, f0, f0et=5.0, f0_refinement_range_cents=10, min_voiced_segment_ms=100):
     """
     Refine the f0 estimate with the help of two-way mismatch algorithm and change the harmonic components
     to the exact multiples of the refined f0 estimate
@@ -118,8 +121,6 @@ def refine_harmonics_twm(hfreq, hmag, hphases, f0, f0et=5.0, f0_refinement_range
     :param f0_refinement_range_cents: the range to be explored in TWM
     :return: new synthesis parameters
     """
-    if len(f0) > len(hfreq):
-        f0 = f0[:len(hfreq)]
     for frame, f0_frame in enumerate(f0):
         if f0_frame > 0:  # for the valid frequencies
             pfreq = hfreq[frame]
@@ -133,7 +134,7 @@ def refine_harmonics_twm(hfreq, hmag, hphases, f0, f0et=5.0, f0_refinement_range
                 hfreq[frame] = 0
                 hmag[frame] = 0
     
-    min_voiced_segment_len = int(np.ceil(0.1/(HOP_SIZE/SAMPLING_RATE)))
+    min_voiced_segment_len = int(np.ceil((min_voiced_segment_ms/1000)/(HOP_SIZE/SAMPLING_RATE)))
     voiced = silence_segments_one_run(f0, 0, min_voiced_segment_len)
     f0[~voiced] = 0
     hfreq[~voiced] = 0
@@ -206,23 +207,29 @@ def analyze_folder(path_folder_audio, path_folder_f0, path_folder_anal, n_jobs=4
 
 
 def process_file(filename, path_folder_audio, path_folder_f0, path_folder_synth, instrument_detector=None):
+    th_lc = 0.3
+    th_hc = 0.7
+    voiced_th_ms = 100
     time_start = taymit()
     audio = librosa.load(os.path.join(path_folder_audio, filename), sr=SAMPLING_RATE, mono=True)[0]
     f0s = pd.read_csv(os.path.join(path_folder_f0, filename[:-3] + "f0.csv"))
-    f0s = silence_unvoiced_segments(f0s, confidence_threshold=0.3, min_voiced_segment_ms=100)
-    f0s = apply_pitch_filter(f0s, min_chunk_size=9, median=True, confidence_threshold=0.75)
+    f0s = silence_unvoiced_segments(f0s, low_confidence_threshold=th_lc, high_confidence_threshold=th_hc,
+                                    min_voiced_segment_ms=voiced_th_ms)
+    f0s = apply_pitch_filter(f0s, min_chunk_size=21, median=True, confidence_threshold=th_hc)
     f0s, conf, time = interpolate_f0_to_sr(f0s, audio)
     time_load = taymit()
     print("loading {:s} took {:.3f}".format(filename, time_load-time_start))
     hfreqs, hmags, hphases = anal(audio, f0s)
+    f0s = f0s[:len(hmags)]
+    conf = conf[:len(hmags)]
+    time = time[:len(hmags)]
     time_anal = taymit()
     print("anal {:s} took {:.3f}".format(filename, time_anal-time_load))
     if instrument_detector is not None:
         hfreqs, hmags, hphases, f0 = supress_timbre_anomalies(instrument_detector, hfreqs, hmags, hphases, f0s)
     hfreqs, hmags, hphases, f0s = refine_harmonics_twm(hfreqs, hmags, hphases,
-                                                       f0s, f0et=5.0, f0_refinement_range_cents=15)
-    time = time[:len(f0s)]
-    conf = conf[:len(f0s)]
+                                                       f0s, f0et=5.0, f0_refinement_range_cents=15,
+                                                       min_voiced_segment_ms=voiced_th_ms)
     time_refine = taymit()
     print("refining parameters for {:s} took {:.3f}. coverage: {:.3f}".format(filename,
                                                                               time_refine-time_anal,
