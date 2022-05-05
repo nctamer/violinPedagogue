d73b1eca25fe9a98205a6d966c4aafddd1c844b9
Nazif Can Tamer <nazifcan.tamer@upf.edu>
2022-05-05 10:58:41 +0200

    Ablation: Bach10 pitch shifts

--- utils/extract_pitch.py
+++ utils/extract_pitch.py
@@ -317,7 +317,7 @@ def urmp_evaluate_all(urmp_path=os.path.join(os.path.expanduser("~"), "violindat
 
 
 if __name__ == '__main__':
-    new_model_name = 'finetuned_instrument_model_50_005_iter2'
+    new_model_name = 'no_pretrain_instrument_model_50_005_iter2'
 
     # Bach10-mf0-synth
     bach10_extract_pitch_with_model(new_model_name, viterbi=False, verbose=1)
