51b9db9d5e54f6fd159fc94e5fc58d9b79922506
Nazif Can Tamer <nazifcan.tamer@upf.edu>
2022-05-07 20:05:47 +0200

    Tables for the paper

--- utils/paper_tables.py
+++ utils/paper_tables.py
@@ -6,5 +6,5 @@ violin_bach = pd.DataFrame(bach10_evaluate_all(instrument='violin')).T
 
 
 non_violin_urmp = pd.DataFrame(urmp_evaluate_all_except_one_instrument(instrument_to_discard='vn', pitch_range=(190,4000))).T
-violin = pd.DataFrame(urmp_evaluate_per_instrument(instrument='vn')).T
+violin_urmp = pd.DataFrame(urmp_evaluate_per_instrument(instrument='vn')).T
 
