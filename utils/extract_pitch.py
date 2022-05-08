6c3b1f92d843cf8448b284544b33f3424c428def
Nazif Can Tamer <nazifcan.tamer@upf.edu>
2022-05-08 10:37:59 +0200

    Iter2 evaluation & extract pitch for iter 3

--- utils/extract_pitch.py
+++ utils/extract_pitch.py
@@ -327,7 +327,7 @@ def urmp_evaluate_all(urmp_path=os.path.join(os.path.expanduser("~"), "violindat
 
 
 if __name__ == '__main__':
-    new_model_name = 'no_pretrain_standard_iter2_mindelta001'
+    new_model_name = 'no_pretrain_standard_iter2'
 
     # Bach10-mf0-synth
     bach10_extract_pitch_with_model(new_model_name, viterbi=False, verbose=1)
